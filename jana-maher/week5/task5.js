5f407bb1fdf280605e1dc8f62061805891bd54ae
Jana maher <janamaher3072006@gmail.com>
2025-08-02 20:37:07 +0300

    Add files via upload

---
+++ jana-maher/week5/task5.js
@@ -0,0 +1,14 @@
+function calculate() {
+  const num1 = parseFloat(document.getElementById("num1").value);
+  const num2 = parseFloat(document.getElementById("num2").value);
+  const operation = document.getElementById("operation").value;
+  let result;
+  if (operation === "add") {
+    result = num1 + num2;
+  } else if (operation === "multiply") {
+    result = num1 * num2;
+  } else {
+    result = "Error" ;
+  }
+   document.getElementById("result").innerText = "Result: " + result;
+}
