6bfdeb31e6f5915b4110f29ca2f6f3158f7dc6a9
Luffycoder0 <luffycoding0@gmail.com>
2025-08-09 18:53:52 +0300

    Update script.js

--- george-sameh/Week 5/script.js
+++ george-sameh/Week 5/script.js
@@ -1,5 +1,6 @@
 function calculate() {
-  const num1 = Number(document.getElementById('num1').value);
+  const num1 = document.getElementById('num1');
+  let Num1 = Number(num1.value);
   const num2 = Number(document.getElementById('num2').value);
   const operation = document.getElementById('operation').value;
   let result;
@@ -12,3 +13,4 @@ function calculate() {
 
   document.getElementById('result').textContent = result;
 }
+
