dabf4d0fc896ca21f95b502356ca2f9498cab2be
KenzieAhmedd <kenzieahmed371@gmail.com>
2025-08-10 18:20:16 +0300

    script.js

---
+++ kenzie-ahmed/week 7/light dark mode toggle task/script.js
@@ -0,0 +1,12 @@
+const button = document.getElementById('toggleButton');
+const body = document.body;
+
+button.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+
+    if (body.classList.contains('dark-mode')) {
+        button.textContent = 'Switch to Light Mode';
+    } else {
+        button.textContent = 'Switch to Dark Mode';
+    }
+});
