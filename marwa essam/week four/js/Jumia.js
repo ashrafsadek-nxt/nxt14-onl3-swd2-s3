97f3d9ea17a7f65564235679581e392b00bbf27a
marwaessam309 <marwaessam087@gmail.com>
2025-08-28 22:59:01 +0300

    Add files via upload

---
+++ marwa essam/week four/js/Jumia.js
@@ -0,0 +1,15 @@
+let offertap =document.getElementById('imgoffer');
+let sell_var = document.getElementById('sell');
+let btn_var = document.getElementById('btn');
+let searchTap_var =document.getElementById('searchTap');
+
+offertap.onclick = function () {
+    location.assign('https://www.jumia.com.eg/')    ;
+}
+sell_var.onclick= function (){
+      location.assign('https://www.https://sell.jumia.com/en/start-now/.com.eg/') ; 
+}
+btn_var.onclick =function () {
+    searchTap_var.focus() ;
+ 
+}
