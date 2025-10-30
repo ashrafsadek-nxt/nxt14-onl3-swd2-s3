963a12b6f1c99a6a58e3f8e5b6c056a8f689c393
Mohanad Refaye <noda.mohanad22@gmail.com>
2025-10-30 14:26:02 +0300

    Add files via upload

---
+++ mohanad-refaye/Week12/task1/src/App.jsx
@@ -0,0 +1,15 @@
+import  './App.css'
+import Welcome from './Welcome.jsx'
+
+function App() {
+   
+  return (
+    <>
+      <div className="container">
+        <Welcome/>
+       </div>
+    </>   
+  )
+}
+
+export default App
